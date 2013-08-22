22db4bb83e49a9b6006de9b6e0fcb079ab93e6ae
Mark Nutter <marknutter@gmail.com>
2013-08-22 17:10:23 -0500

    fixed user creation

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -21,28 +21,13 @@ class RegistrationsController < Devise::RegistrationsController
       end
     else
       clean_up_passwords resource
-      respond_with resource
-    end
-  end
+        @user = resource
+        @current_user = current_user
+        respond_to do |format|
 
-  # PUT /resource
-  # We need to use a copy of the resource because we don't want to change
-  # the current user in place.
-  def update
-    self.resource = resource_class.to_adapter.get!(send(:"current_#{resource_name}").to_key)
-    prev_unconfirmed_email = resource.unconfirmed_email if resource.respond_to?(:unconfirmed_email)
-
-    if resource.update_with_password(account_update_params)
-      if is_navigational_format?
-        flash_key = update_needs_confirmation?(resource, prev_unconfirmed_email) ?
-          :update_needs_confirmation : :updated
-        set_flash_message :notice, flash_key
-      end
-      sign_in resource_name, resource, :bypass => true
-      respond_with resource, :location => after_update_path_for(resource)
-    else
-      clean_up_passwords resource
-      respond_with resource
+          format.html # new.html.erb
+          format.json { render 'users/_user' }
+        end
     end
   end
 
